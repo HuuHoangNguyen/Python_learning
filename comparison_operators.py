f8a54c47541eb39fe30b754b19b023828a547ad2
HuuHoang Nguyen <hoangnh.hpp@gmail.com>
2016-08-22 13:03:57 +0700

    Update code 2016-08-22 13h05

---
+++ comparison_operators.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+a = 21
+b = 10
+c = 0
+#-----------------------------------------------------------------
+if (a == b) :
+    print "Line 1: a is equal to b"
+else:
+    print "Line 1: a is not equal to b"
+
+#-----------------------------------------------------------------
+if (a != b):
+    print "Line 2: a is not equal to b"
+else:
+    print "Line 2: a is equal to b"
+
+#-----------------------------------------------------------------
+if ( a <> b):
+    print "Line 3: a is not equal to b"
+else:
+    print "Line 3: a is equal to b"
+
+#-----------------------------------------------------------------
+if ( a < b):
+    print " Line 4: a is less than b"
+else:
+    print  "Line 4: a is not less than b "
+
+if ( a > b):
+    print "Line 5: a is greater than b"
+else:
+    print "Line 5: a is not greater than b"
+
+a = 5
+b = 20
+if (a <= b):
+    print "Line 6: a is either less than or equal to b"
+else:
+    print "Line 6: a is not either less than or equal to b"
+
+if ( a >= b):
+    print "Line 7: a is either greater than or equal to b"
+else:
+    print "Line 7: a is not either greater than or equal to b"
