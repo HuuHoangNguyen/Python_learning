13cbcd5d2e725380b1f8a6c9317801d6bfe167fa
HuuHoangNguyen <hoangnh.hpp@gmail.com>
2016-08-25 00:54:20 +0700

    Update code 2016-08-25 00h55

--- mathematical_function.py
+++ mathematical_function.py
@@ -1,6 +1,66 @@
 #!/usr/bin/python
 
 print "============================================================"
+print "abs()"
+print "abs(-45) :", abs(-45)
+print "abs (100.12) : ", abs(100.12)
+print "abs(119L) : ", abs(119L)
 
+print "============================================================"
+print "ceil(x): Return the ceil of x, the smallest integer not less than x"
+import math
+print "math.ceil(-45.17) : ", math.ceil(-45.17)
+print "math.ceil(100.12) : ", math.ceil(100.12)
+print "math.ceil(100.72) : ", math.ceil(100.72)
+print "math.ceil(119L) : ", math.ceil(119L)
+print "makth.ceil(math.pi) : ", math.ceil(math.pi)
+
+print "============================================================"
+print "cmp(x, y): -1 if x < y, o if x = y, or 1 if x > y"
+print "cmp(80, 100) : ", cmp(80, 100)
+print "cmp(180, 100) : ", cmp(180, 180)
+print "cmp(-80, 100) : ", cmp(-80, 100)
+print "cmp(80, -100) : ", cmp(80, -100)
+
+print "============================================================"
+print "exp(x): The exponential of x: e^x"
+print "math.exp(-45.17) : ", math.exp(-57.17)
+print "math.exp(100.12) : ", math.exp(100.12)
+print "math.exp(100.72) : ", math.exp(100.72)
+print "math.exp(119L) : ", math.exp(119L)
+print "math.exp(math.pi) :", math.exp(math.pi)
+
+print "============================================================"
+print "fabs(x) the absolute value of x"
+print "math.fabs(-45.17) : ", math.fabs(-45.17)
+print "math.fabs(100.12) : ", math.fabs(100.12)
+print "math.fabs(100.72) : ", math.fabs(100.72)
+print "math.fabs(119L) : ", math.fabs(119L)
+print "math.fabs(math.pi) : ", math.fabs(math.pi)
+
+print "============================================================"
+print "floor(x): The floor of x: the largest integer not greater than x"
+print "math.floor(-45.17) : ", math.floor(-45.17)
+print "math.floor(100.12) : ", math.floor(100.12)
+print "math.floor(100.72) : ", math.floor(100.72)
+print "math.floor(119L) : ", math.floor(119L)
+print "math.floor(math.pi) : ", math.floor(math.pi)
+
+print "============================================================"
+print "log(x): the natural logarithm of x, for x > 0: loge(x)"
+print "math.log(100.12) : ", math.log(100.12)
+print "math.log(100.72) : ", math.log(100.72)
+print "math.log(119L) : ", math.log(119L)
+print "math.log(math.pi) : ", math.log(math.pi)
+
+print "============================================================"
+print "============================================================"
+
+print "============================================================"
+
+print "============================================================"
+print "============================================================"
+
+print "============================================================"
 
 print "============================================================"
