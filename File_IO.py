5623d9a0c91d9f9469a4a7720e50c8869b9a5521
HuuHoangNguyen <hoangnh.hpp@gmail.com>
2016-09-16 15:04:35 +0700

    Update 2016-09-16 15h00

--- File_IO.py
+++ File_IO.py
@@ -57,5 +57,19 @@ call other support methods associated with it.
 Syntax:
     file object = open(file_name [, access_mod] [, buffering])
     
+Here are parameter detail:
+    + file_name: The file_name argument is a string value that contains the name of 
+                 the file that you want to access.
+    + access_mode: The access_mode determines the mode in which the file haas to be opened. 
+                   i.e read, write, append, etc. A complete list of possible value s is given
+                   below in the table. This is optional parameter and the default file access 
+                   mode is read(r)
+
+    + buffering: If the buffering value is set to 0, no buffering takes place. If the buffering 
+    value is 1, line buffering is performed while accessing a file. If you specify the buffering 
+    value as an integer greater than 1, then buffering action is performed with the indicated buffer size.
+    If negative, the buffer size is the system default( default behavior)
+
+
 """
 
