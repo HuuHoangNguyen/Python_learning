1dd00eb8c49095d0307cfcd41a032137bd90beaf
HuuHoang Nguyen <hoangnh.hpp@gmail.com>
2016-09-19 11:00:00 +0700

    Update 2016-09-19 10h55

--- Expections.py
+++ Expections.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python
+
+"""
+The assert Statement
+When it encounter an assert statement, Python evaluates  the accompanying expression, 
+Which is hopefully true. If the expression is false, Pyhon raises an AssertionError exception.
+
+assert Expression[, Arguments]
+
+If the assertion fails, Python uses ArgumentExpression as the argument for the AssertionError. 
+AsseerError exceptions can be caught and handled like any other exception using the try-except statement,
+but if not handled, they will teminate the program adnf produce a traceback.
+
+"""
+
+def Kelvin2Fahrenheit(Temperature):
+    assert (Temperature >= 0), "Colder than absolute zero!"
+    return (((Temperature - 273) * 1.8 ) + 32)
+
+print Kelvin2Fahrenheit(273)
+print int(Kelvin2Fahrenheit(505.78))
+print Kelvin2Fahrenheit(-5)
+
+
+"""
+Exception
+Syntax:
+    try:
+        #You do your oprations here.
+    except ExceptionI:
+        #If the is ExceptionI, then execute this block
+    except ExceptionII:
+        #If the is ExceptionI, then execute this block
+    ......................
+    else:
+        If there is no exception then execute this block. 
+"""     
